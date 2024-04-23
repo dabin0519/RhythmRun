5a8fa5f84d7b422da44ebae22983b6907c65a31a
dabin0519 <dadabin0519@naver.com>
2024-04-23 20:28:10 +0900

    ui finish

--- RhythmRun/Assets/01_Scripts/00_Manager/UIContoller.cs
+++ RhythmRun/Assets/01_Scripts/00_Manager/UIContoller.cs
@@ -2,15 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum UIType
+{
+    OPTION,
+    SCORE
+}
+
+[System.Serializable]
+public struct UIPanel
+{
+    public UIType type;
+    public GameObject panel;
+}
+
 public class UIContoller : MonoSingleton<UIContoller>
 {
     [SerializeField] private AudioClip _onMouseClip;
     [SerializeField] private AudioClip _clickMouseClip;
-    [SerializeField] private GameObject _scorePanel;
+    [SerializeField] private UIPanel[] _uiPanels;
 
     private void Awake()
     {
-        _scorePanel.SetActive(false);
+        UIPanelOff();
+    }
+
+    private void UIPanelOff()
+    {
+        foreach(var p in _uiPanels)
+        {
+            p.panel.SetActive(false);
+        }
     }
 
     public void PlayMouseOnSound()
@@ -27,7 +48,19 @@ public class UIContoller : MonoSingleton<UIContoller>
 
     public void ScorePanelOn()
     {
-        _scorePanel.SetActive(true);
+        GetPanel(UIType.SCORE).SetActive(true);
         GameManager.Instance.Pause(true);
+    }
+
+    private GameObject GetPanel(UIType type)
+    {
+        foreach(var p in _uiPanels)
+        {
+            if(p.type == type)
+            {
+                return p.panel;
+            }
+        }
+        return null;
     }
 }
--- RhythmRun/Assets/01_Scripts/UI/Score/ScorePanel.cs
+++ RhythmRun/Assets/01_Scripts/UI/Score/ScorePanel.cs
@@ -1,8 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScorePanel : MonoBehaviour
 {
-    
+    [SerializeField] private Transform _levelGroupTrm;
+    [SerializeField] private Transform _textGroupTrm;
+    [SerializeField] private Transform _starGrounTrm;
+
+    // C~S까지 티어
+    private Image _fillTierImage;
+    private TextMeshProUGUI _tierText;
+
+    //TextGroup(perfect, normal, bad 등)
+    private TextMeshProUGUI _perfectText;
+    private TextMeshProUGUI _goodText;
+    private TextMeshProUGUI _normalText;
+    private TextMeshProUGUI _badText;
+    private TextMeshProUGUI _missText;
+    private TextMeshProUGUI _hiddenNoteText;
+    private TextMeshProUGUI _scoreText;
+
+    //Star
+    private Image[] _starImages;
+
+    private void Awake()
+    {
+        // tier
+        _fillTierImage = _levelGroupTrm.Find("FillTierImage").GetComponent<Image>();
+        _tierText = _levelGroupTrm.Find("TierText").GetComponent<TextMeshProUGUI>();
+
+        // text
+        _perfectText        = _textGroupTrm.Find("PerfectText").GetComponent<TextMeshProUGUI>();
+        _goodText           = _textGroupTrm.Find("GoodText").GetComponent<TextMeshProUGUI>();
+        _normalText         = _textGroupTrm.Find("NormalText").GetComponent<TextMeshProUGUI>();
+        _badText            = _textGroupTrm.Find("BadText").GetComponent<TextMeshProUGUI>();
+        _missText           = _textGroupTrm.Find("MissText").GetComponent<TextMeshProUGUI>();
+        _hiddenNoteText     = _textGroupTrm.Find("HiddenNote").GetComponent<TextMeshProUGUI>();
+        _scoreText          = _textGroupTrm.Find("ScoreText").GetComponent<TextMeshProUGUI>();
+
+        // star
+        _starImages         = _starGrounTrm.GetComponentsInChildren<Image>();
+    }
+
+    public void UpdateScorePanel()
+    {
+        
+    }
 }
